fdc031f0e8cd169ec83bc6e6f681c2cc8cac8537
elorenzen <eric.lorenzen@gmail.com>
2019-05-25 13:38:46 -0600

    Added Semantic-UI classes to component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 class SearchBar extends React.Component{
     render() {
-        return <div>Search Bar</div>
+        return(
+            <div className='ui segment'>
+                <form className='ui form'>
+                    <input type='text' className='field' />
+                </form>
+            </div>
+        ) 
     }
 };
 
