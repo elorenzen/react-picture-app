6bf7a2c72bf7f2b417643ebe50bb4cd8a4b2afa1
elorenzen <eric.lorenzen@gmail.com>
2019-05-25 23:36:06 -0600

    Added App prop into onFormSubmit function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,9 +3,11 @@ import React from 'react';
 class SearchBar extends React.Component{
     state = { term: '' };
 
-    onFormSubmit(event) {
+    onFormSubmit = (event) => {
         // Prevents form from submitting on automatic refresh
         event.preventDefault();
+
+        this.props.onSubmit(this.state.term);
     }
 
     render() {
