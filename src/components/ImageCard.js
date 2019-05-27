02caaf8cbf2ef1c6947fd35d4cceaf14acc3d4ac
elorenzen <eric.lorenzen@gmail.com>
2019-05-27 10:05:32 -0600

    Added constructor, componentDidMount and arrow function that prints height of each picture to the console

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
-    render() {
+    constructor(props) {
+        super(props);
 
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+
+    setSpans = () => {
+        console.log(this.imageRef.current.clientHeight);
+    }
+
+    render() {
         const { alt_description, urls} = this.props.image;
 
         return (
             <div>
                 <img 
+                    ref={this.imageRef}
                     src={urls.regular} 
                     alt={alt_description} 
                 />
